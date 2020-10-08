afe2fe17ed57bc735de1dfea4f412d428ac6dc5f
Daniel Vinther Wolf <danielvinther@youmail.dk>
2020-10-08 12:05:08 +0200

    Add files for inital commit

---
+++ Ladeskab.Test.Unit/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace Ladeskab.Test.Unit
+{
+    public class LadeskabUnitTests
+    {
+
+        private ThisIsATestClass _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Ladeskab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ladeskab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class ThisIsATestClass
+    {
+        public bool ThisWillReturnTrue()
+        {
+            return true;
+        }
+    }
+}
