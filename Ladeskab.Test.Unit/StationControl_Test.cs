e1d0066429ad053503f87910b25db5da3780e94a
Daniel Vinther Wolf <danielvinther@youmail.dk>
2020-10-08 15:05:52 +0200

    Add project files and fix references

---
+++ Ladeskab.Test.Unit/StationControl_Test.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace Ladeskab.Test.Unit
+{
+    public class ChargeControlUnitTest
+    {
+        // Pre-Setup:
+        //private ThisIsATestClass _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Common Arrange:
+            // _uut = new ThisIsATestClass();
+            
+        }
+
+        [Test]
+        public void ThisWillReturnTrue_NoAction_IsTrue()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            //Assert.IsTrue(_uut.ThisWillReturnTrue());
+            Assert.IsTrue(true);
+        }
+    }
+}
--- Ladeskab/Program.cs
+++ Ladeskab/Program.cs
@@ -1,20 +1,48 @@
 ﻿using System;
 
-namespace Ladeskab
-{
-    class Program
+namespace Ladeskab {
+
+class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+				// Assemble your system here from all the classes
+                /* OUTCOMMENTED by DANIEL
+            bool finish = false;
+            do
+            {
+                string input;
+                System.Console.WriteLine("Indtast E, O, C, R: ");
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) continue;
 
-    public class ThisIsATestClass
-    {
-        public bool ThisWillReturnTrue()
-        {
-            return true;
+                switch (input[0])
+                {
+                    case 'E':
+                        finish = true;
+                        break;
+
+                    case 'O':
+                        door.OnDoorOpen();
+                        break;
+
+                    case 'C':
+                        door.OnDoorClose();
+                        break;
+
+                    case 'R':
+                        System.Console.WriteLine("Indtast RFID id: ");
+                        string idString = System.Console.ReadLine();
+
+                        int id = Convert.ToInt32(idString);
+                        rfidReader.OnRfidRead(id);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (!finish);*/
         }
     }
 }
