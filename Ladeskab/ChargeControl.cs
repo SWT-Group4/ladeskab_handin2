97dfab2517d32f02dddf0ecbdc697c3e8448a06a
Daniel Vinther Wolf <danielvinther@youmail.dk>
2020-10-12 19:18:08 +0200

    Reorganize files to make the app only project

---
+++ Ladeskab/ChargeControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UsbSimulator;
+
+namespace Ladeskab
+{
+    public class ChargeControl : IUsbCharger
+    {
+
+        public event EventHandler<CurrentEventArgs> CurrentValueEvent;
+        public double CurrentValue { get; }
+        public bool Connected { get; }
+        public void StartCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopCharge()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
