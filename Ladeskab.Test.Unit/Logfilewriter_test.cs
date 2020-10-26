2b2c648eae23e73a9cf467c4678304799cd1f7be
Malthe Brauer-Nielsen <au608380@uni.au.dk>
2020-10-26 12:15:36 +0100

    Added setup for tests and began work on first test to logging

--- Ladeskab.Test.Unit/Logfilewriter_test.cs
+++ Ladeskab.Test.Unit/Logfilewriter_test.cs
@@ -1,10 +1,28 @@
-﻿using NUnit.Framework;
+﻿using System.Runtime.InteropServices.ComTypes;
+using LadeskabClasses;
+using LadeskabClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
 
 namespace Ladeskab.Test.Unit
 {
     [TestFixture]
     public class Logfilewriter_test
     {
-        
+        private ILogfileWriter _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = Substitute.For<ILogfileWriter>();
+        }
+
+
+        [TestCase(1)]
+        public void LoggingWhen_DoorUnlocked(int x)
+        {
+            _uut.LogDoorLocked(x);
+        }
+
     }
 }
