54592c027f66bb9ba115898c576a87c2089734da
Malthe Brauer-Nielsen <au608380@uni.au.dk>
2020-10-26 09:19:16 +0100

    Added extra test files

---
+++ Ladeskab.Test.Unit/Logfilewriter_test.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace Ladeskab.Test.Unit
+{
+    [TestFixture]
+    public class Logfilewriter_test
+    {
+        
+    }
+}
---
+++ Ladeskab.Test.Unit/RFIDReader_test.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace Ladeskab.Test.Unit
+{
+    [TestFixture]
+    public class RFIDReader_test
+    {
+        
+    }
+}
