caf3466fe1e00ae4b3d6849cd27c0e126f478975
Malthe Brauer-Nielsen <au608380@uni.au.dk>
2020-10-26 18:48:42 +0100

    Added tests for all DisplaySimulators functions

--- Ladeskab.Test.Unit/Display_Test.cs
+++ Ladeskab.Test.Unit/Display_Test.cs
@@ -19,26 +19,129 @@ namespace Ladeskab.Test.Unit
     public class DisplayUnitTest
     {
         // Pre-Setup:
+        private DisplaySimulator uut;
+        private IDisplayOutput output;
 
-
-        [SetUp]
+            [SetUp]
         public void Setup()
         {
             // Common Arrange:
+            output = Substitute.For<IDisplayOutput>();
+            uut = new DisplaySimulator(output);
 
         }
 
 
 
         [Test]
-        public void a_b_c()
+        public void NotCharging_Printsmessage()
         {
             // Arrange
 
             // Act
-
+            uut.NotCharging();
             // Assert
-            Assert.IsTrue(true);
+            output.Received().PrintToDisplay(Arg.Is<string>(str => str.Contains("Device is Not Charging")));
         }
+
+        [Test]
+        public void FullyCharged_Printsmessage()
+        {
+            // Arrange
+
+            // Act
+            uut.FullyCharged();
+            // Assert
+            output.Received().PrintToDisplay(Arg.Is<string>(str => str.Contains("Device is Fully Charged")));
+        }
+
+        [Test]
+        public void IsCharging_Printsmessage()
+        {
+            // Arrange
+
+            // Act
+            uut.IsCharging();
+            // Assert
+            output.Received().PrintToDisplay(Arg.Is<string>(str => str.Contains("Device Is now Charging")));
+        }
+
+        [Test]
+        public void OverCurrentFail_Printsmessage()
+        {
+            // Arrange
+
+            // Act
+            uut.OverCurrentFail();
+            // Assert
+            output.Received().PrintToDisplay(Arg.Is<string>(str => str.Contains("FAILURE: Over Current")));
+        }
+
+        [Test]
+        public void StateChangedToLocked_Printsmessage()
+        {
+            // Arrange
+
+            // Act
+            uut.StateChangedToLocked();
+            // Assert
+            output.Received().PrintToDisplay(Arg.Is<string>(str => str.Contains("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.")));
+        }
+
+        [Test]
+        public void ErrorInPhoneConnection_Printsmessage()
+        {
+            // Arrange
+
+            // Act
+            uut.ErrorInPhoneConnection();
+            // Assert
+            output.Received().PrintToDisplay(Arg.Is<string>(str => str.Contains("Din telefon er ikke ordentlig tilsluttet. Prøv igen.")));
+        }
+
+        [Test]
+        public void StateChangedToUnlocked_Printsmessage()
+        {
+            // Arrange
+
+            // Act
+            uut.StateChangedToUnlocked();
+            // Assert
+            output.Received().PrintToDisplay(Arg.Is<string>(str => str.Contains("Tag din telefon ud af skabet og luk døren")));
+        }
+
+        [Test]
+        public void RfidNoMatch_Printsmessage()
+        {
+            // Arrange
+
+            // Act
+            uut.RfidNoMatch();
+            // Assert
+            output.Received().PrintToDisplay(Arg.Is<string>(str => str.Contains("Forkert RFID tag")));
+        }
+
+        [Test]
+        public void ConnectPhone_Printsmessage()
+        {
+            // Arrange
+
+            // Act
+            uut.ConnectPhone();
+            // Assert
+            output.Received().PrintToDisplay(Arg.Is<string>(str => str.Contains("Tilslut telefon")));
+        }
+
+        [Test]
+        public void ReadRfid_Printsmessage()
+        {
+            // Arrange
+
+            // Act
+            uut.ReadRfid();
+            // Assert
+            output.Received().PrintToDisplay(Arg.Is<string>(str => str.Contains("Indlæs RFID")));
+        }
+
     }
 }
